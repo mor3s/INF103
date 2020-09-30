c0042412dbb84d017a8ef2fe7762346d5ed56847
Mor3s <nour.boulahcen@gmail.com>
2020-09-30 14:39:51 +0200

    TP1-part2 Class Student

--- TP1/src/Student.java
+++ TP1/src/Student.java
@@ -1,2 +1,41 @@
 public class Student {
+    private final int id;
+    private String firstname;
+    private String lastname;
+
+    public Student(int id, String firstname, String lastname) {
+        super();
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 }
---
+++ TP1/src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student(1024,"Patrick","Bellot");
+        System.out.println(student.toString());
+
+    }
+}
